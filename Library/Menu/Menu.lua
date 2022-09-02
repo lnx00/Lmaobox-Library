a14f3f84bee84d35df544cec80ee1b6a525c6fec
lnx00 <linch255@gmail.com>
2022-09-03 00:21:02 +0200

    Added buttons to menu

--- Library/Menu/Menu.lua
+++ Library/Menu/Menu.lua
@@ -1,6 +1,9 @@
 ---@type Fonts
 local Fonts = require("Library/UI/Fonts")
 
+---@type KeyHelper
+local KeyHelper = require("Library/Utils/KeyHelper")
+
 ---@class Menu
 ---@field public Cursor table
 ---@field public CursorStack table
@@ -9,13 +12,23 @@ local Menu = {
     CursorStack = Stack.new()
 }
 
+local MouseHelper = KeyHelper.new(MOUSE_LEFT)
+
 local Windows = { }
 local WindowStack = Stack.new()
 
 local Style = {
-    TitleSize = 20,
-    Padding = 4
+    TitleHeight = 20,
+    TitleColor = { 55, 100, 215, 255 },
+    Padding = 4,
+    TextColor = { 255, 255, 255, 255 },
+    WindowColor = { 30, 30, 30, 255 },
 }
+
+function MouseInBound(x, y, w, h)
+    local mX, mY = table.unpack(input.GetMousePos())
+    return mX >= x and mX <= x + w and mY >= y and mY <= y + h
+end
 
 function Menu.Begin(title)
     if not Windows[title] then
@@ -25,37 +38,32 @@ function Menu.Begin(title)
     local window = Windows[title]
 
     -- Mouse drag
-    if input.IsButtonDown(MOUSE_LEFT) then
+    if MouseInBound(window.X, window.Y, window.Width, Style.TitleHeight) and MouseHelper:Down() then
         local mX, mY = table.unpack(input.GetMousePos())
 
-        if mX > window.X and mX < window.X + window.Width and mY > window.Y and mY < window.Y + Style.TitleSize then
-            window.X = math.floor(mX - window.Width / 2)
-            window.Y = math.floor(mY - Style.TitleSize / 2)
-        end
+        window.X = math.floor(mX - window.Width / 2)
+        window.Y = math.floor(mY - Style.TitleHeight / 2)
     end
 
-    Menu.Cursor.X = window.X + Style.Padding
-    Menu.Cursor.Y = window.Y + Style.TitleSize + Style.Padding
+    Menu.Cursor.X = window.X
+    Menu.Cursor.Y = window.Y + Style.TitleHeight + Style.Padding
 
     draw.SetFont(Fonts.Verdana)
 
     -- Background
-    draw.Color(30, 30, 30, 255)
+    draw.Color(table.unpack(Style.WindowColor))
     draw.FilledRect(window.X, window.Y, window.X + window.Width, window.Y + window.Height)
 
     -- Title bar
-    draw.Color(55, 100, 215, 255)
-    draw.FilledRect(window.X, window.Y, window.X + window.Width, window.Y + Style.TitleSize)
+    draw.Color(table.unpack(Style.TitleColor))
+    draw.FilledRect(window.X, window.Y, window.X + window.Width, window.Y + Style.TitleHeight)
 
     -- Title text
-    draw.Color(255, 255, 255, 255)
+    draw.Color(table.unpack(Style.TextColor))
     local titleWidth, titleHeight = draw.GetTextSize(title)
     draw.Text(math.floor(window.X + (window.Width / 2) - (titleWidth / 2)), window.Y + math.floor((20 / 2) - (titleHeight / 2)), title)
 
-    draw.Color(255, 255, 255, 255)
     WindowStack:push(window)
-
-    -- TODO: Update WindowPos on drag
 end
 
 function Menu.End()
@@ -65,9 +73,38 @@ end
 
 function Menu.Text(text)
     local width, height = draw.GetTextSize(text)
+
+    draw.Color(table.unpack(Style.TextColor))
     draw.Text(Menu.Cursor.X, Menu.Cursor.Y, text)
 
     Menu.Cursor.Y = Menu.Cursor.Y + height + Style.Padding
+end
+
+function Menu.Button(text)
+    local txtWidth, txtHeight = draw.GetTextSize(text)
+    local width, height = txtWidth + Style.Padding * 2, txtHeight + Style.Padding * 2
+    local x, y = Menu.Cursor.X, Menu.Cursor.Y
+    local hovered = MouseInBound(x, y, width, height)
+    local clicked = MouseHelper:Pressed()
+
+    -- Background
+    if hovered then
+        if MouseHelper:Down() then
+            draw.Color(70, 70, 70, 255)
+        else
+            draw.Color(60, 60, 60, 255)
+        end
+    else
+        draw.Color(50, 50, 50, 255)
+    end
+    draw.FilledRect(x, y, x + width, y + height)
+
+    -- Text
+    draw.Color(255, 255, 255, 255)
+    draw.Text(math.floor(x + (width / 2) - (txtWidth / 2)), math.floor(y + (height / 2) - (txtHeight / 2)), text)
+
+    Menu.Cursor.Y = Menu.Cursor.Y + height + Style.Padding
+    return hovered and clicked
 end
 
 return Menu
