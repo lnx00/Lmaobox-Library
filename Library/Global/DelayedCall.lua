c6bcf223772fa93ed32fdad6d731d4f2c7015695
lnx00 <linch255@gmail.com>
2023-01-07 19:27:59 +0100

    Fixed error messages in DelayedCalls

--- Library/Global/DelayedCall.lua
+++ Library/Global/DelayedCall.lua
@@ -2,6 +2,7 @@
     Delayed Calls
 ]]
 
+---@type { time : number, func : fun() }[]
 local delayedCalls = {}
 
 -- Calls the given function after the given delay
@@ -10,7 +11,7 @@ local delayedCalls = {}
 function _G.DelayedCall(delay, func)
     table.insert(delayedCalls, {
         time = globals.RealTime() + delay,
-        thread = coroutine.create(func)
+        func = func
     })
 end
 
@@ -20,8 +21,8 @@ local function OnDraw()
     local curTime = globals.RealTime()
     for i, call in ipairs(delayedCalls) do
         if curTime > call.time then
-            coroutine.resume(call.thread)
             table.remove(delayedCalls, i)
+            call.func()
         end
     end
 end
