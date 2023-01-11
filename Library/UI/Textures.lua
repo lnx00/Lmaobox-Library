fa896ab0f9cf762bcdad296300b23f3989d8c04f
lnx00 <linch255@gmail.com>
2023-01-11 21:15:17 +0100

    Optimized gradient generator

--- Library/UI/Textures.lua
+++ Library/UI/Textures.lua
@@ -10,37 +10,46 @@ for i = 0, 255 do
     byteMap[i] = string.char(i)
 end
 
----@param data table
----@return string
-local function TableToBinary(data)
-    return table.concat(data)
+local function GetColor(color)
+    local r, g, b, a = table.unpack(color)
+    a = a or 255
+    return r, g, b, a
 end
 
--- [PERFORMANCE INTENSIVE] Creates a linear gradient (255 pixels or greater)
+local function GetSize(size)
+    local w, h = table.unpack(size)
+    w, h = w or 255, h or 255
+    return w, h
+end
+
+-- [PERFORMANCE INTENSIVE] Creates a linear gradient
 ---@param startColor table<number, number, number, number>
 ---@param endColor table<number, number, number, number>
 ---@param size table<number, number>
 ---@return string
 function Textures.LinearGradient(startColor, endColor, size)
-    local sR, sG, sB, sA = table.unpack(startColor)
-    local eR, eG, eB, eA = table.unpack(endColor)
-    sA, eA = sA or 255, eA or 255
-    local width = size[1] or 255
-    local height = size[2] or 255
+    local sR, sG, sB, sA = GetColor(startColor)
+    local eR, eG, eB, eA = GetColor(endColor)
+    local w, h = GetSize(size)
 
+    local dataSize = w * h * 4
     local data = {}
-    for y = 1, height do
-        local yh = y / height
-        for x = 1, width do
-            local xw = x / width
-            table.insert(data, byteMap[math.floor(sR + (eR - sR) * xw)])
-            table.insert(data, byteMap[math.floor(sG + (eG - sG) * yh)])
-            table.insert(data, byteMap[math.floor(sB + (eB - sB) * xw)])
-            table.insert(data, byteMap[math.floor(sA + (eA - sA) * yh)])
-        end
+    local bm = byteMap
+    
+    local i = 1
+    while i < dataSize do
+        local idx = (i / 4)
+        local x, y = idx % w, idx // w
+
+        data[i] = bm[sR + (eR - sR) * x // w]
+        data[i + 1] = bm[sG + (eG - sG) * y // h]
+        data[i + 2] = bm[sB + (eB - sB) * x // w]
+        data[i + 3] = bm[sA + (eA - sA) * y // h]
+
+        i = i + 4
     end
 
-    return TableToBinary(data)
+    return table.concat(data)
 end
 
 -- [PERFORMANCE INTENSIVE] Creates a circle with a given color
