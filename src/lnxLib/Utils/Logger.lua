6fe27d0048047f5152a20f40283200fcb7045e11
titanium_machine <78664175+titaniummachine1@users.noreply.github.com>
2024-01-30 00:09:27 +0100

    Update Logger.lua

--- src/lnxLib/Utils/Logger.lua
+++ src/lnxLib/Utils/Logger.lua
@@ -5,50 +5,47 @@
 ---@class Logger
 ---@field public Name string
 ---@field public Level integer
----@field public Debug fun(...)
----@field public Info fun(...)
----@field public Warn fun(...)
----@field public Error fun(...)
-local Logger = {
-    Name = "",
-    Level = 1
-}
+local Logger = {}
 Logger.__index = Logger
-setmetatable(Logger, Logger)
 
--- Creates a new logger
 ---@param name string
 ---@return Logger
 function Logger.new(name)
     local self = setmetatable({}, Logger)
-    self.Name = name
+    self.Name = name or "Logger"
     self.Level = 1
-
     return self
 end
 
----@type table<string, { Color:table, Level:integer }>
 local logModes = {
     ["Debug"] = { Color = { 165, 175, 190 }, Level = 0 },
-    ["Info"] = { Color = { 15, 185, 180 }, Level = 1 },
-    ["Warn"] = { Color = { 225, 175, 45 }, Level = 2 },
+    ["Info"]  = { Color = { 15, 185, 180 }, Level = 1 },
+    ["Warn"]  = { Color = { 225, 175, 45 }, Level = 2 },
     ["Error"] = { Color = { 230, 65, 25 }, Level = 3 }
 }
 
+local function getCurrentTime()
+    -- Update the timestamp at most once per second.
+    local currentTime = os.time()
+    if not Logger.lastTime or currentTime > Logger.lastTime then
+        Logger.lastFormattedTime = os.date("%H:%M:%S")
+        Logger.lastTime = currentTime
+    end
+    return Logger.lastFormattedTime
+end
+
 -- Initialize the log methods dynamically
 for mode, data in pairs(logModes) do
-    rawset(Logger, mode, function(self, ...)
+    Logger[mode] = function(self, ...)
         if data.Level < self.Level then return end
 
-        local msg = string.format(...)
-
+        local msg = ...
         local r, g, b = table.unpack(data.Color)
-        local name = self.Name
-        local time = os.date("%H:%M:%S")
+        local time = getCurrentTime()
 
-        local logMsg = string.format("[%-6s%s] %s: %s", mode, time, name, msg)
+        local logMsg = "[" .. mode .. " " .. time .. "] " .. self.Name .. ": " .. msg
         printc(r, g, b, 255, logMsg)
-    end)
+    end
 end
 
 return Logger
